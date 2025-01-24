3aae6fa12ce77418db8203d23adbec2ad2335b07
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-01-24 21:47:47 +0200

    -

---
+++ main/main.gd
@@ -0,0 +1,48 @@
+#@tool
+class_name Main
+extends Control
+
+#region Signals
+#endregion
+
+#region Enums
+#endregion
+
+#region Constants
+#endregion
+
+#region Export Variables
+#endregion
+
+#region Public Variables
+#endregion
+
+#region Private Variables
+#endregion
+
+#region OnReady Variables
+#endregion
+
+#region Virtual Methods
+#endregion
+
+#region Public Methods
+#endregion
+
+#region Private Methods
+#endregion
+
+#region Static Methods
+#endregion
+
+#region Signal Callbacks
+#endregion
+
+#region SubClasses
+#endregion
+
+#region Setter Methods
+#endregion
+
+#region Getter Methods
+#endregion
