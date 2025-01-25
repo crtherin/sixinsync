0dc775286159c2fbab07b60607a8437aed75f699
DarkToilet99 <ane.mac99@gmail.com>
2025-01-25 22:56:52 +0200

    Advanced mumbler manager
    -supports multiple dialogues at the same time

---
+++ resources/Mumble/mumble_manager.gd
@@ -0,0 +1,35 @@
+extends Node
+
+@export var path: String
+var mumblers: Array[Mumbler]
+var _streams: Array[Resource]
+
+func _ready():
+	var dir =DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_names = dir.get_files()
+		for file in file_names:
+			if(file.ends_with('.wav')):
+				_streams.append(load(path + file))
+
+func mumble(text:String):
+	var mumbler = Mumbler.getInstance(text, path, _streams)
+	self.add_child(mumbler)
+	mumbler.stringToMorsePop()
+	mumblers.append(mumbler)
+	
+func skip():
+	for mumbler in mumblers:
+		mumbler.skip = true
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var key_input := event as InputEventKey
+		
+		if key_input.pressed:
+			match key_input.keycode:
+				KEY_0: mumble('Stalin e cel mai tare') #ForTestPurposes
+				KEY_SPACE: skip()
+				
+				
