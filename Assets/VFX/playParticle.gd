21716aacbf7589c87fa8e03f9e7f59d10083891f
DarkToilet99 <ane.mac99@gmail.com>
2025-01-26 15:37:41 +0200

    More vfx

--- Assets/VFX/playParticle.gd
+++ Assets/VFX/playParticle.gd
@@ -7,11 +7,3 @@ func play() -> void:
 	particles.restart()
 	playSound.emit()
 	
-	
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		var key_input := event as InputEventKey
-		
-		if key_input.pressed:
-			match key_input.keycode: #ForTestPurposes
-				KEY_SPACE: play()
