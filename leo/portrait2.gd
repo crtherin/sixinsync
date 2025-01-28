5dcb46267345e8c1886a3bda97d933776305d89a
Kanasz Mihai (uic02628) <mihai.kanasz@continental-corporation.com>
2025-01-28 16:33:22 +0200

    Deprecated file, commented lines to avoid errors

--- leo/portrait2.gd
+++ leo/portrait2.gd
@@ -9,17 +9,17 @@ func _ready():
 	texture_normal = defaultIcon
 
 
-func _on_portrait_pressed():		
-	var customer = Global.getCurrentCustomer()
-	if customer:
-		var texture = load(customer.get("icon"))
-		if texture:
-			texture_normal = texture
-		else:
-			texture_normal = defaultIcon
-		chat_bubble.show_bubble(customer.get("description"))
+#func _on_portrait_pressed():		
+	#var customer = Global.customerArray
+	#if customer:
+		#var texture = load(customer.get("icon"))
+		#if texture:
+			#texture_normal = texture
+		#else:
+			#texture_normal = defaultIcon
+		#chat_bubble.show_bubble(customer.get("description"))
 
 
-func _on_reset_pressed():
-	Global.resetCustomer()
-	_on_portrait_pressed()
+#func _on_reset_pressed():
+	#Global.resetCustomer()
+	#_on_portrait_pressed()
